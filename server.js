f0db77cfd1c3b6d887249914f6c77268e8f3c758
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-02-25 17:15:41 -0500

    code development

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static directory
+app.use(express.static("public"));
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -19,6 +22,9 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
-});
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+    });
+  });
+  
