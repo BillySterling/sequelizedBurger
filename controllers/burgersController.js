77632c2585c0428e46365ac51bed6789157a7477
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-03-01 18:05:20 -0500

    continuing code updates

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,41 +1,36 @@
 var express = require("express");
-
+var db = require("../models");
 var router = express.Router();
-var burger = require("../models/burger.js");
 
-// get route -> index
+debugger;
 router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+  db.Burgers.findAll().then(function(burgerData) {
     res.render("index", { burger_data: burgerData });
   });
 });
 
-// post route -> back to index
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
+  db.Burgers.create({
+    id: null,
+    burger_name: req.body.burger_name,
+    devoured: false
+    });
     res.redirect("/");
   });
-});
 
-// put route -> back to index
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    // Send back response and let page reload from .then in Ajax
-    res.sendStatus(200);
+router.put("/burgers/update", function(req, res) {
+  db.Burgers.update({
+    devoured: true
+  }, {
+    where: {
+      id: req.body.burger_id
+    }
   });
-});
+    res.redirect("/");
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var db = require('./models');
+var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -10,11 +12,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("public"));
-
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -22,9 +19,8 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    });
   });
-  
+});
